77ced89de363e4597f048072502800a79027243d
lemon-shark-25 <196343170+lemon-shark-25@users.noreply.github.com>
2026-04-10 18:32:35 +0200

    Added test file

---
+++ src/mp/practicaimplementacion/ClasePrueba.java
@@ -0,0 +1,15 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package mp.practicaimplementacion;
+
+/**
+ *
+ * @author Ignacio Jerónimo Martín i.jeronimo.2024@alumnos.urjc.es
+ */
+public class ClasePrueba {
+	public void prueba(){
+		System.out.println("Hola");
+	}
+}
