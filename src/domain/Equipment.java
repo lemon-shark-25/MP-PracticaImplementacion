ba0e0dc7c21ed2365f2838c0f098d7d60e9bc176
lemon-shark-25 <196343170+lemon-shark-25@users.noreply.github.com>
2026-04-17 17:31:57 +0200

    Añdir Equipo y Personaje.

---
+++ src/domain/Equipment.java
@@ -0,0 +1,16 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package domain;
+
+/**
+ *
+ * @author Ignacio Jerónimo Martín i.jeronimo.2024@alumnos.urjc.es
+ */
+public abstract Equipment {
+	private String name;
+	private int attackModifier;
+	private int defenseModifier;
+
+}
