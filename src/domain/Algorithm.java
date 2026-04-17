ff03801dde9dec41f9691df451e594068eeb9820
lemon-shark-25 <196343170+lemon-shark-25@users.noreply.github.com>
2026-04-17 17:57:34 +0200

    Añadir interfaz AlgoritmAñadir interfaz Algoritmoo

---
+++ src/domain/Algorithm.java
@@ -0,0 +1,14 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package domain;
+
+/**
+ * Algoritmos. Implementan el patrón Strategy.
+ * 
+ * @author Ignacio Jerónimo Martín i.jeronimo.2024@alumnos.urjc.es
+ */
+public interface Algorithm {
+	public int execute(Character c);
+}
