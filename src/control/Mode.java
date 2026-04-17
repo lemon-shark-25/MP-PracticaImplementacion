abdeb6db5a7f4683bf096d7061fe65d2f0a9f294
lemon-shark-25 <196343170+lemon-shark-25@users.noreply.github.com>
2026-04-17 17:50:41 +0200

    Añadir Mode y Screen

---
+++ src/control/Mode.java
@@ -0,0 +1,17 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package control;
+
+import intereaction.Screen;
+
+/**
+ *
+ * @author Ignacio Jerónimo Martín i.jeronimo.2024@alumnos.urjc.es
+ */
+public class Mode {
+	public void showScreen(Screen s){
+		s.showScreen();
+	}	
+}
