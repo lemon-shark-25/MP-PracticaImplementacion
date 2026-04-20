fd1b1164dda8bb708cecaac7065d53ce5cd02722
Usuario <Usuario@DESKTOP-V7PBR7S>
2026-04-20 19:20:59 +0200

    "terminado demon"

---
+++ src/domain/Demon.java
@@ -0,0 +1,61 @@
+
+package domain;
+
+import java.util.Iterator;
+import java.util.LinkedList;
+
+/**
+ *
+ * @author Usuario
+ */
+public class Demon implements Minion{
+    private String name;
+    private int health;
+    private Character owner;
+    private String pact;
+    private LinkedList<Minion> minions;
+
+    public Demon(String name, int health, Character owner, String pact, LinkedList<Minion> minions) {
+        this.name = name;
+        this.health = health;
+        this.owner = owner;
+        this.pact = pact;
+        this.minions = minions;
+    }
+
+    public Character getOwner() {
+        return owner;
+    }
+    public String getPact() {
+        return pact;
+    }
+
+    public LinkedList<Minion> getMinions() {
+        return minions;
+    }
+
+    @Override
+    public String getName() {
+        return name;
+    }
+
+    @Override
+    public int getHealth() {
+        int x = 0;
+        Iterator<Minion> it = minions.iterator();
+        while (it.hasNext()) {
+            Minion m = it.next(); 
+            x += m.getHealth(); 
+        }
+        x = x + this.health;
+        return x;
+    }
+    
+    public void addMinion(Minion minion){
+        minions.add(minion);
+    }
+    
+    public void removeMinion(Minion minion){
+        minions.remove(minion);
+    }
+}
