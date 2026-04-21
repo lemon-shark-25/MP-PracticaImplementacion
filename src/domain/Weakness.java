909e17d64ada813876eacee7058df3554b0a2d68
Usuario <Usuario@DESKTOP-V7PBR7S>
2026-04-21 21:03:17 +0200

    "añadido type a weakness"

--- src/domain/Weakness.java
+++ src/domain/Weakness.java
@@ -8,10 +8,12 @@ package domain;
 public class Weakness implements Modifier{
     private String name;
     private int weaknessValue;
+    private int type;
 
-    public Weakness(String name, int weaknessValue) {
+    public Weakness(String name, int weaknessValue, int type) {
         this.name = name;
         this.weaknessValue = weaknessValue;
+        this.type = type;
     }
     
     @Override
@@ -22,6 +24,11 @@ public class Weakness implements Modifier{
     @Override
     public int getValue() {
         return weaknessValue;
+    }
+
+    @Override
+    public int getType() {
+        return type;
     }
     
     
