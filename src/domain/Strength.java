584a0b3baa4983e9accdddc3eafa03df1115b1f1
Usuario <Usuario@DESKTOP-V7PBR7S>
2026-04-23 17:02:52 +0200

    "Arreglo de la clase strength que hereda de modifier"

--- src/domain/Strength.java
+++ src/domain/Strength.java
@@ -5,30 +5,9 @@ package domain;
  *
  * @author Hugo Martínez González
  */
-public class Strength implements Modifier {
-    private String name;
-    private int StrengthValue;
-    private int type;
-
-    public Strength(String name, int StrengthValue, int type) {
-        this.name = name;
-        this.StrengthValue = StrengthValue;
-        this.type = type;
-    }
+public class Strength extends Modifier {
     
-    @Override
-    public String getName() {
-        return name;
+    public Strength(String name, int valuee) {
+        super(name,valuee);
     }
-
-    @Override
-    public int getValue() {
-        return StrengthValue;
-    }
-
-    @Override
-    public int getType() {
-        return type;
-    }
-    
 }
