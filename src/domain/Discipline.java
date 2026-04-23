70bd91c1cc36fccc20732ff1cb0f940da8faaa2d
Usuario <Usuario@DESKTOP-V7PBR7S>
2026-04-23 17:10:52 +0200

    "Arreglo de la clase Discipline"

--- src/domain/Discipline.java
+++ src/domain/Discipline.java
@@ -5,34 +5,14 @@ package domain;
  *
  * @author Hugp Martínez González
  */
-public class Discipline implements Ability{
-    private String name;
-    private int attackValue;
-    private int defenseValue;
-    private int bloodValue;
+public class Discipline extends Ability{
+    private final int bloodValue;
 
     public Discipline(String name, int attackValue, int defenseValue, int bloodValue) {
-        this.name = name;
-        this.attackValue = attackValue;
-        this.defenseValue = defenseValue;
-        this.bloodValue = bloodValue;
+        super(name, attackValue,defenseValue);
+            this.bloodValue = bloodValue;
     }
-   
-    @Override
-    public String getName() {
-        return name;
-    }
-
-    @Override
-    public int getAttackValue() {
-        return  attackValue;
-    }
-
-    @Override
-    public int getDefenseValue() {
-        return defenseValue;
-    }
-
+    
     public int getBloodValue() {
         return bloodValue;
     }
