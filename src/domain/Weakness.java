bff858c0fcc29f474112db237bd80b34ea43fd99
Usuario <Usuario@DESKTOP-V7PBR7S>
2026-04-23 17:02:29 +0200

    "Arreglo de la clase weakness que hereda de modifier"

--- src/domain/Weakness.java
+++ src/domain/Weakness.java
@@ -5,31 +5,9 @@ package domain;
  *
  * @author Hugo Martínez González
  */
-public class Weakness implements Modifier{
-    private String name;
-    private int weaknessValue;
-    private int type;
+public class Weakness extends Modifier{
 
-    public Weakness(String name, int weaknessValue, int type) {
-        this.name = name;
-        this.weaknessValue = weaknessValue;
-        this.type = type;
-    }
-    
-    @Override
-    public String getName() {
-        return name;
-    }
-
-    @Override
-    public int getValue() {
-        return weaknessValue;
-    }
-
-    @Override
-    public int getType() {
-        return type;
-    }
-    
-    
+    public Weakness(String name, int valuee ) {
+        super(name,valuee);
+    }    
 }
