8928dbc014df02c2f74c4338c1341753cbecd6e8
Usuario <Usuario@DESKTOP-V7PBR7S>
2026-04-23 17:13:46 +0200

    "Arreglo de la clase Gift"

--- src/domain/Gift.java
+++ src/domain/Gift.java
@@ -5,27 +5,9 @@ package domain;
  *
  * @author Hugo Martínez González
  */
-public class Gift implements Ability{
-    private String name;
-    private int attackValue;
-    private int defenseValue;
-
+public class Gift extends Ability{
+  
     public Gift(String name, int attackValue, int defenseValue) {
-        this.name = name;
-        this.attackValue = attackValue;
-        this.defenseValue = defenseValue;
+        super(name,attackValue,defenseValue);
     }
-
-    public String getName() {
-        return name;
-    }
-
-    public int getAttackValue() {
-        return attackValue;
-    }
-
-    public int getDefenseValue() {
-        return defenseValue;
-    }
-        
 }
