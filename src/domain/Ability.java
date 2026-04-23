05449d944b84e74d0d8fd81aed8b6553e890548f
Usuario <Usuario@DESKTOP-V7PBR7S>
2026-04-23 17:08:28 +0200

    "cambio de interfaz a clase abstracta"

--- src/domain/Ability.java
+++ src/domain/Ability.java
@@ -5,8 +5,26 @@ package domain;
  *
  * @author Hugo Martínez González
  */
-public interface Ability {
-    String getName();
-    int getAttackValue();
-    int getDefenseValue();
+public abstract class Ability {
+	private final String name;
+	private final int attackValue;
+        private final int defenseValue;
+
+	public Ability(String name, int attack, int defense){
+		this.name = name;
+		attackValue = attack;
+                defenseValue = defense;
+	}
+
+	public String getName() {
+		return name;
+	}
+
+	public int getAttackValue() {
+		return attackValue;
+	}	
+        
+        public int getDefenseValue() {
+		return defenseValue;
+	}
 }
