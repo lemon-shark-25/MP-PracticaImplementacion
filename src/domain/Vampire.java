d852b6a1ac8231d2a1ce142dfa953084d6b8d3d8
MikePradiBart <m.pradillo.2020@alumnos.urjc.es>
2026-04-24 18:06:57 +0200

    Class Vampire V2 (setters deleted)

--- src/domain/Vampire.java
+++ src/domain/Vampire.java
@@ -5,17 +5,16 @@ import java.util.LinkedList;
 
 /**
  * Clase que representa a la raza Vampiro dentro del juego.
- * * @author Miguel Pradillo Bartolomé
+ * @author Miguel Pradillo Bartolomé
  */
 public class Vampire extends Character {
 
-    // Atributos específicos del Vampiro según el enunciado
     private int bloodPoints;
     private int age;
 
     /**
      * Constructor completo del Vampiro.
-     * Pedimos explícitamente un objeto 'Discipline' en lugar de un 'Ability' genérico.
+     * (Será llamado internamente por el VampireBuilder en su método build())
      */
     public Vampire(String name, int health, int power, Discipline discipline, 
                    HashMap<String, Armor> armor, HashMap<String, Weapons> weapon, 
@@ -23,35 +22,30 @@ public class Vampire extends Character {
                    Equipment principalArmor, Equipment principalWeapon, 
                    int bloodPoints, int age) {
         
-        // Invocamos al constructor de la clase padre (Character)
-        // Pasamos 'discipline' al parámetro 'ability' del padre
         super(name, health, power, discipline, armor, weapon, minion, strength, weakness, principalArmor, principalWeapon);
-        
         this.bloodPoints = bloodPoints;
         this.age = age;
     }
 
-    // --- Getters y Setters específicos ---
+    // --- Getters ---
 
     public int getBloodPoints() {
         return bloodPoints;
-    }
-
-    public void setBloodPoints(int bloodPoints) {
-        this.bloodPoints = bloodPoints;
     }
 
     public int getAge() {
         return age;
     }
 
+    // --- Setter único ---
+    
     public void setAge(int age) {
         this.age = age;
     }
 
     /**
      * Sobrescribimos el getter de Ability para que devuelva directamente 
-     * una Discipline y no haya que hacer casteos manuales en el combate.
+     * una Discipline y evitar conversiones manuales en el combate.
      */
     @Override
     public Discipline getAbility() {
