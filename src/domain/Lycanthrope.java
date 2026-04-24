84aadea8f3828b62a5243f4c33a1651336be2e3e
Usuario <Usuario@DESKTOP-V7PBR7S>
2026-04-24 17:56:28 +0200

    "Supuestamente terminada la clase Lycantrhope"

--- src/domain/Lycanthrope.java
+++ src/domain/Lycanthrope.java
@@ -1,10 +1,51 @@
 
 package domain;
 
+import java.util.HashMap;
+import java.util.LinkedList;
+
 /**
  *
  * @author Hugo Martínez González
  */
-public class Lycanthrope {
+public class Lycanthrope extends Character {
+    private int heigth;
+    private int weigth;
+    private int rage;
+
+    public Lycanthrope(int heigth, int weigth, int rage, String name, 
+            int health, int power, Will will, HashMap<String, Armor> armor, HashMap<String, Weapons> weapon, 
+            Minion minion, LinkedList<Strength> strength, LinkedList<Weakness> weakness, 
+            Equipment principalArmor, Equipment principalWeapon) {
+        super(name, health, power, will, armor, weapon, minion, strength, weakness, principalArmor, principalWeapon);
+        this.heigth = heigth;
+        this.weigth = weigth;
+        this.rage = rage;
+    }
+
+    public int getHeigth() {
+        return heigth;
+    }
+
+    public int getWeigth() {
+        return weigth;
+    }
+
+    public int getRage() {
+        return rage;
+    }
+
+    public void setHeigth(int heigth) {
+        this.heigth = heigth;
+    }
+
+    public void setWeigth(int weigth) {
+        this.weigth = weigth;
+    }
+    
+    @Override
+    public Will getAbility() {
+        return (Will) super.getAbility();
+    }
     
 }
