7c8bc1914d3cb2e04c3726e2668fb36241c55905
Usuario <Usuario@DESKTOP-V7PBR7S>
2026-04-27 13:30:24 +0200

    "Cambiado el HunterCreator"

--- src/domain/HunterCreator.java
+++ src/domain/HunterCreator.java
@@ -1,19 +1,15 @@
-/*
- * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
- * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
- */
+
 package domain;
 
 /**
  *
  * @author Hugo Martínez González
  */
-public class HunterCreator implements CharacterCreator {
+public class HunterCreator implements GameCharacterCreator {
 
     @Override
-    public Character create() {
-        return new Hunter();
+    public Hunter create() {
+        return (Hunter) new Hunter();
     }
-    
-    
+       
 }
