03728d61c417ce73f31140d2cb8a0470011d10f3
Usuario <Usuario@DESKTOP-V7PBR7S>
2026-04-27 17:54:06 +0200

    "Arreglado weakness"

--- src/domain/Strength.java
+++ src/domain/Strength.java
@@ -7,7 +7,7 @@ package domain;
  */
 public class Strength extends Modifier {
     
-    public Strength(String name, int valuee, int type) {
-        super(name,valuee, type);
+    public Strength(String name, String desc, int valuee, int type) {
+        super(name, desc, valuee, type);
     }
 }
