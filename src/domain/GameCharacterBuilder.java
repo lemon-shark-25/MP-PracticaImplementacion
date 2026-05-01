7eaad3a81dd70cf3f92656aca4be38dbbf864fce
Usuario <Usuario@DESKTOP-V7PBR7S>
2026-05-01 17:12:33 +0200

    "cambio en el builder"

---
+++ src/domain/GameCharacterBuilder.java
@@ -0,0 +1,10 @@
+package domain;
+
+/**
+ * Interfaz para el patrón Builder aplicado a la creación de personajes.
+ * * * @author Miguel Pradillo Bartolomé
+ */
+public interface GameCharacterBuilder {
+    
+    GameCharacter gameCharacterBuilder();
+}
