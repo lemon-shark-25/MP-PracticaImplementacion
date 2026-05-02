773c25a1b32162afdbe006edf8f7ab3930854d85
Usuario <Usuario@DESKTOP-V7PBR7S>
2026-05-02 13:46:45 +0200

    "Quitado minion de las herederas"

--- src/domain/LycanthropeCharacterBuilder.java
+++ src/domain/LycanthropeCharacterBuilder.java
@@ -13,8 +13,8 @@ public class LycanthropeCharacterBuilder extends SimilarGameCharacterBuilder {
     private int heigth;
     private int weigth;
 
-    public LycanthropeCharacterBuilder(HashMap<String, Ability> ability, HashMap<String, Armor> armor, HashMap<String, Weapons> weapon, Minion minion, HashMap<String, Strength> strength, HashMap<String, Weakness> weakness) {
-        super(ability, armor, weapon, minion, strength, weakness);
+    public LycanthropeCharacterBuilder(HashMap<String, Ability> ability, HashMap<String, Armor> armor, HashMap<String, Weapons> weapon, HashMap<String, Strength> strength, HashMap<String, Weakness> weakness) {
+        super(ability, armor, weapon, strength, weakness);
     }
  
     @Override
