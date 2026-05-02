3047d5f7c217358b62ed0cce01152fc80eaf6d55
Usuario <Usuario@DESKTOP-V7PBR7S>
2026-05-02 14:17:59 +0200

    "arreglando herecia y abilidades de hunter"

--- src/domain/HunterCharacterBuilder.java
+++ src/domain/HunterCharacterBuilder.java
@@ -2,6 +2,7 @@
 package domain;
 
 import java.util.HashMap;
+import java.util.Scanner;
 
 /**
  *
@@ -9,7 +10,7 @@ import java.util.HashMap;
  */
 public class HunterCharacterBuilder extends SimilarGameCharacterBuilder{
     
-    public HunterCharacterBuilder(HashMap<String, Ability> ability, HashMap<String, Armor> armor, HashMap<String, Weapons> weapon, HashMap<String, Strength> strength, HashMap<String, Weakness> weakness) {
+    public HunterCharacterBuilder(HashMap<String, ? extends Ability> ability, HashMap<String, Armor> armor, HashMap<String, Weapons> weapon, HashMap<String, Strength> strength, HashMap<String, Weakness> weakness) {
         super(ability, armor, weapon, strength, weakness);
     }
  
@@ -18,6 +19,15 @@ public class HunterCharacterBuilder extends SimilarGameCharacterBuilder{
         Hunter hunterr =(Hunter) super.gameCharacterBuilder(characterr);
         return hunterr;
   
+    }
+    
+    @Override
+    protected void setAbility(GameCharacter characterr, Scanner sc){
+        System.out.println("Escoge la habilidad especial de tu personaje:");
+        String[] inventary = showOptions(ability,sc,false,"habilidad");
+        String name = inventary[requestNumber("Escoge la habilidad de tu personaje pulsando el número", 0, inventary.length-1, sc)];
+        Will wil = (Will) ability.get(name);
+        characterr.setAbility(wil); 
     }
 
 }
