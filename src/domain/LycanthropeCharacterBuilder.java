4c20d012c14bab1cc57e6ab09b33d5359f5846a0
Usuario <Usuario@DESKTOP-V7PBR7S>
2026-05-02 14:16:56 +0200

    "arreglando herecia y abilidades de licantropo"

--- src/domain/LycanthropeCharacterBuilder.java
+++ src/domain/LycanthropeCharacterBuilder.java
@@ -13,7 +13,7 @@ public class LycanthropeCharacterBuilder extends SimilarGameCharacterBuilder {
     private int heigth;
     private int weigth;
 
-    public LycanthropeCharacterBuilder(HashMap<String, Ability> ability, HashMap<String, Armor> armor, HashMap<String, Weapons> weapon, HashMap<String, Strength> strength, HashMap<String, Weakness> weakness) {
+    public LycanthropeCharacterBuilder(HashMap<String, ? extends Ability> ability, HashMap<String, Armor> armor, HashMap<String, Weapons> weapon, HashMap<String, Strength> strength, HashMap<String, Weakness> weakness) {
         super(ability, armor, weapon, strength, weakness);
     }
  
@@ -29,5 +29,12 @@ public class LycanthropeCharacterBuilder extends SimilarGameCharacterBuilder {
   
     }
     
-    
+    @Override
+    protected void setAbility(GameCharacter characterr, Scanner sc){
+        System.out.println("Escoge la habilidad especial de tu personaje:");
+        String[] inventary = showOptions(ability,sc,false,"habilidad");
+        String name = inventary[requestNumber("Escoge la habilidad de tu personaje pulsando el número", 0, inventary.length-1, sc)];
+        Gift gif = (Gift) ability.get(name);
+        characterr.setAbility(gif); 
+    }
 }
