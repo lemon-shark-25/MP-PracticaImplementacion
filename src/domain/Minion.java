e450da0922238ad12d350ac9b03b97d29844d180
Usuario <Usuario@DESKTOP-V7PBR7S>
2026-05-03 21:25:42 +0200

    "Poniendo serializable"

--- src/domain/Minion.java
+++ src/domain/Minion.java
@@ -1,11 +1,13 @@
 
 package domain;
 
+import java.io.Serializable;
+
 /**
  *
  * @author Hugo Martínez González
  */
-public interface Minion {
+public interface Minion extends Serializable {
      String getName();
      int getHealth();
 }
